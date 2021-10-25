140dd5c423534fc1d3559de7107a8305f7cf2cd7
dumganhar <dumganhar@gmail.com>
2021-10-25 15:40:10 +0800

    Add asset.jsb.ts, effect-asset.jsb.ts, material.jsb.ts

---
+++ cocos/core/assets/material.jsb.ts
@@ -0,0 +1,84 @@
+/*
+ Copyright (c) 2021 Xiamen Yaji Software Co., Ltd.
+
+ http://www.cocos.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+  worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+  not use Cocos Creator software for developing other software or tools that's
+  used for developing games. You are not granted to publish, distribute,
+  sublicense, and/or sell copies of Cocos Creator.
+
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+/**
+ * @packageDocumentation
+ * @module material
+ */
+import { EffectAsset } from './effect-asset';
+import { Texture } from '../gfx';
+import { TextureBase } from './texture-base';
+import { legacyCC } from '../global-exports';
+import { PassOverrides } from '../renderer/core/pass';
+import { MacroRecord, MaterialProperty, PropertyType } from '../renderer/core/pass-utils';
+
+/**
+ * @en The basic infos for material initialization.
+ * @zh 用来初始化材质的基本信息。
+ */
+interface IMaterialInfo {
+    /**
+     * @en The EffectAsset to use. Must provide if `effectName` is not specified.
+     * @zh
+     * 这个材质将使用的 EffectAsset，直接提供资源引用，和 `effectName` 至少要指定一个。
+     */
+    effectAsset?: EffectAsset | null;
+    /**
+     * @en
+     * The name of the EffectAsset to use. Must provide if `effectAsset` is not specified.
+     * @zh
+     * 这个材质将使用的 EffectAsset，通过 effect 名指定，和 `effectAsset` 至少要指定一个。
+     */
+    effectName?: string;
+    /**
+     * @en
+     * The index of the technique to use.
+     * @zh
+     * 这个材质将使用第几个 technique，默认为 0。
+     */
+    technique?: number;
+    /**
+     * @en
+     * The shader macro definitions. Default to 0 or the specified value in [[EffectAsset]].
+     * @zh
+     * 这个材质定义的预处理宏，默认全为 0，或 [[EffectAsset]] 中的指定值。
+     */
+    defines?: MacroRecord | MacroRecord[];
+    /**
+     * @en
+     * The override values on top of the pipeline states specified in [[EffectAsset]].
+     * @zh
+     * 这个材质的自定义管线状态，将覆盖 effect 中的属性。<br>
+     * 注意在可能的情况下请尽量少的自定义管线状态，以减小对渲染效率的影响。
+     */
+    states?: PassOverrides | PassOverrides[];
+}
+
+type MaterialPropertyFull = MaterialProperty | TextureBase | Texture | null;
+
+export type Material = jsb.Material;
+export const Material = jsb.Material;
+
+legacyCC.Material = Material;
