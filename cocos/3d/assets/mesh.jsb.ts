9c71017578dc714735804686fa0b4e0e7c9e604d
Patrice Jiang <397136899@qq.com>
2021-10-26 18:37:20 +0800

    update some types

---
+++ cocos/3d/assets/mesh.jsb.ts
@@ -0,0 +1,14 @@
+export declare namespace Mesh {
+    export interface IBufferView {
+        offset: number;
+        length: number;
+        count: number;
+        stride: number;
+    }
+    export type IVertexBundle = jsb.Mesh.IVertexBundle;
+    export type ISubMesh = jsb.Mesh.ISubMesh;
+    export type IStruct = jsb.Mesh.IStruct;
+    export type ICreateInfo = jsb.Mesh.ICreateInfo;
+}
+export type Mesh = jsb.Mesh;
+export const Mesh = jsb.Mesh;
