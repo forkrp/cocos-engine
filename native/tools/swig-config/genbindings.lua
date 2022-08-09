427f5023b38cff527ba9c9b93326d4c7f7d64235
James Chen <dumganhar@gmail.com>
2022-08-09 18:36:35 +0800

    Update genbindings.lua/.sh

--- native/tools/swig-config/genbindings.lua
+++ native/tools/swig-config/genbindings.lua
@@ -1,24 +1,31 @@
 local PATH = os.getenv('PATH')
 local SWIG_LIB = os.getenv('SWIG_LIB')
+local SWIG_LIB2 = os.getenv('SWIG_LIB2')
+local SWIG_LIB3 = os.getenv('SWIG_LIB3')
+local SWIG_EXE = os.getenv('SWIG_EXE')
 local COCOS_NATIVE_ROOT = os.getenv('COCOS_NATIVE_ROOT')
-local SWIG_ROOT = os.getenv('SWIG_ROOT')
 
-local all_dirs = {
+local all_paths = {
     SWIG_LIB,
+    SWIG_LIB2,
+    SWIG_LIB3,
+    SWIG_EXE,
     COCOS_NATIVE_ROOT,
-    SWIG_ROOT
 }
 
 --- Check if a file or directory exists in this path
 function exists(file)
-   local ok, err, code = os.rename(file, file)
-   if not ok then
-      if code == 13 then
-         -- Permission denied, but it exists
-         return true
-      end
-   end
-   return ok, err
+    if file == nil then
+        return false
+    end
+    local ok, err, code = os.rename(file, file)
+    if not ok then
+        if code == 13 then
+            -- Permission denied, but it exists
+            return true
+        end
+    end
+    return ok, err
 end
 
 --- Check if a directory exists in this path
@@ -27,14 +34,16 @@ function isdir(path)
    return exists(path.."/")
 end
 
-print('COCOS_NATIVE_ROOT: ' .. COCOS_NATIVE_ROOT)
-print('PATH: ' .. PATH)
-print('SWIG_LIB: ' .. SWIG_LIB)
-print('SWIG_ROOT: ' .. SWIG_ROOT)
+print('COCOS_NATIVE_ROOT: ' .. tostring(COCOS_NATIVE_ROOT))
+print('PATH: ' .. tostring(PATH))
+print('SWIG_LIB: ' .. tostring(SWIG_LIB))
+print('SWIG_LIB2: ' .. tostring(SWIG_LIB2))
+print('SWIG_LIB3: ' .. tostring(SWIG_LIB3))
+print('SWIG_EXE: ' .. tostring(SWIG_EXE))
 
-for _, dir in ipairs(all_dirs) do
-    if not isdir(dir) then
-        print(string.format('(%s) does not exist!', dir))
+for _, path in ipairs(all_paths) do
+    if path ~= nil and not exists(path) then
+        print(string.format('(%s) does not exist!', path))
         return
     end
 end
@@ -60,10 +69,29 @@ local swig_config_map = {
 }
 
 for _, config in ipairs(swig_config_map) do
-	local command = string.format('%s %s %s %s %s', 
-		'swig -c++ -cocos -fvirtual -noexcept -cpperraswarn',
+    local includes = {
+        SWIG_LIB,
+        SWIG_LIB2,
+        SWIG_LIB3,
+        COCOS_NATIVE_ROOT,
+        COCOS_NATIVE_ROOT .. '/cocos',
+    }
+
+    local includeStrArr = {}
+    for i = 1, #includes + 1 do
+        local v = includes[i]
+        if exists(v) then
+            table.insert(includeStrArr, '-I' .. v)
+        end
+    end
+
+    local includeStr = table.concat(includeStrArr, " ")
+
+	local command = string.format('%s %s %s %s %s %s', 
+        SWIG_EXE,
+		'-c++ -cocos -fvirtual -noexcept -cpperraswarn',
 		'-D__clang__ -Dfinal= -DCC_PLATFORM=3 -Dconstexpr=const -DCC_PLATFORM_ANDROID=3',
-		'-I' .. SWIG_ROOT .. '/build' .. ' -I' .. COCOS_NATIVE_ROOT .. ' -I' .. COCOS_NATIVE_ROOT .. '/cocos',
+		includeStr,
 		'-o ' .. COCOS_NATIVE_ROOT .. '/cocos/bindings/auto/' .. config[2],
 		COCOS_NATIVE_ROOT .. '/tools/swig-config/' .. config[1]
 	)
--- native/tools/swig-config/genbindings.sh
+++ native/tools/swig-config/genbindings.sh
@@ -12,9 +12,26 @@ export COCOS_NATIVE_ROOT=$( realpath "$DIR/../../" )
 
 echo $COCOS_NATIVE_ROOT
 
-export SWIG_ROOT=/Users/james/Project/cocos/swig
-# export SWIG_ROOT=/home/james/projects/swig
-export PATH=$SWIG_ROOT/build:$SWIG_ROOT/build/Release:$PATH
-export SWIG_LIB=$SWIG_ROOT/Lib
+
+# mac
+
+# SWIG_ROOT=/Users/james/Project/cocos/swig
+# export SWIG_EXE=$SWIG_ROOT/build/Release/swig
+
+# linux
+
+SWIG_ROOT=/home/james/projects/swig
+
+# linux release
+
+export SWIG_EXE=$SWIG_ROOT/build/install/bin/swig
+export SWIG_LIB=$SWIG_ROOT/build/install/share/swig/4.1.0
+
+# linux debug
+
+# export SWIG_EXE=$SWIG_ROOT/build/swig
+# export SWIG_LIB=$SWIG_ROOT/build
+# export SWIG_LIB2=$SWIG_ROOT/Lib/javascript/cocos
+# export SWIG_LIB3=$SWIG_ROOT/Lib
 
 ./lua-${host_os} genbindings.lua
