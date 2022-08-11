f0212dc3afff98ebc79073512c0dcc4a4cc7393f
James Chen <dumganhar@gmail.com>
2022-08-11 11:44:06 +0800

    genbindings.sh

--- native/tools/swig-config/genbindings.sh
+++ native/tools/swig-config/genbindings.sh
@@ -8,14 +8,22 @@ host_os=`uname -s | tr "[:upper:]" "[:lower:]"`
 echo $host_os
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export COCOS_NATIVE_ROOT=$( realpath "$DIR/../../" )
+
+function abspath() { pushd . > /dev/null; if [ -d "$1" ]; then cd "$1"; dirs -l +0; else cd "`dirname \"$1\"`"; cur_dir=`dirs -l +0`; if [ "$cur_dir" == "/" ]; then echo "$cur_dir`basename \"$1\"`"; else echo "$cur_dir/`basename \"$1\"`"; fi; fi; popd > /dev/null; }
+
+export COCOS_NATIVE_ROOT=$( abspath "$DIR/../../" )
 
 echo $COCOS_NATIVE_ROOT
-
 
 # mac
 
 if [ "$host_os" == "darwin" ]; then
+    # release
+    # SWIG_ROOT=/Users/jianhua/projects/swig-mac
+    # export SWIG_EXE=$SWIG_ROOT/bin/swig
+    # export SWIG_LIB=$SWIG_ROOT/share/swig/4.1.0
+
+    # debug
     SWIG_ROOT=/Users/james/Project/cocos/swig
     export SWIG_EXE=$SWIG_ROOT/build/Release/swig
     export SWIG_LIB=$SWIG_ROOT/build
