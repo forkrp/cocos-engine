836080f4c135bcbd74c7a1839b3fa44b26aab226
James Chen <dumganhar@gmail.com>
2022-08-11 15:52:42 +0800

    Update

--- native/tools/swig-config/genbindings.bat
+++ native/tools/swig-config/genbindings.bat
@@ -7,9 +7,9 @@ for %%i in ("%COCOS_NATIVE_ROOT%") do SET COCOS_NATIVE_ROOT=%%~fi
 
 echo COCOS_NATIVE_ROOT=%COCOS_NATIVE_ROOT%
 
-set SWIG_ROOT=C:/projects/swig-install
+set SWIG_ROOT=%COCOS_NATIVE_ROOT%/external/win64/bin/swig
 
-set SWIG_EXE=%SWIG_ROOT%/bin/swig
+set SWIG_EXE=%SWIG_ROOT%/bin/swig.exe
 set SWIG_LIB=%SWIG_ROOT%/share/swig/4.1.0
 
 .\lua.exe genbindings.lua
