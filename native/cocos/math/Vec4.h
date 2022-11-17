410ccd5d618b11c684f38fe0d23214de578b1690
James Chen <dumganhar@gmail.com>
2022-11-17 10:59:03 +0800

    Format code.

--- native/cocos/math/Vec4.h
+++ native/cocos/math/Vec4.h
@@ -444,8 +444,8 @@ public:
         return math::isEqualF(x, v.x, precision) && math::isEqualF(y, v.y, precision) && math::isEqualF(z, v.z, precision) && math::isEqualF(w, v.w, precision);
     }
 
-    template<class Archive>
-    void serialize(Archive& ar) {
+    template <class Archive>
+    void serialize(Archive &ar) {
         ar.serialize(x, "x");
         ar.serialize(y, "y");
         ar.serialize(z, "z");
